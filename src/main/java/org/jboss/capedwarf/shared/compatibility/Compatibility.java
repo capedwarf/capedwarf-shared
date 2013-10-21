a1390d6b3e5e8fb0181c86582ef378f91151be49
Ales Justin <ales.justin@gmail.com>
2013-10-22 00:13:26 +0200

    Add default GCS bucket name prop.

--- src/main/java/org/jboss/capedwarf/shared/compatibility/Compatibility.java
+++ src/main/java/org/jboss/capedwarf/shared/compatibility/Compatibility.java
@@ -62,7 +62,8 @@ public class Compatibility {
         ENABLE_SOCKET_OPTIONS("enable.socket.options"),
         IGNORE_CAPEDWARF_SOCKETS("ignore.capedwarf.sockets"),
         IGNORE_CAPEDWARF_URL_STREAM_HANDLER("ignore.capedwarf.url.stream.handler"),
-        CHANNEL_DEFAULT_DURATION_MINUTES("channel.default.duration.minutes", new IntegerValue(2 * 60));
+        CHANNEL_DEFAULT_DURATION_MINUTES("channel.default.duration.minutes", new IntegerValue(2 * 60)),
+        DEFAULT_GCS_BUCKET_NAME("default.gcs.bucket.name", new NotEmpty());
 
         private String key;
         private Value value;
